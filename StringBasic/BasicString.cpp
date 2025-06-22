fac8ab13fc4549578b2bfb1088db0868fc11d3a4
uri <13288uri@gmail.com>
2025-06-22 12:18:08 +0300

    Add testing in main

--- StringBasic/BasicString.cpp
+++ StringBasic/BasicString.cpp
@@ -1,6 +1,20 @@
 #include "BasicString.h"
 
-explicit BasicString::BasicString(const char* const data) {
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void BasicString::copyBasicString(const BasicString& copied) {
+	m_size = copied.m_size;
+	size_t i = 0;
+	m_data = new char[m_size];
+	for (i = 0; i < m_size; i++) {
+		m_data[i] = copied.m_data[i];
+	}
+}
+
+BasicString::BasicString(const char* const data) {
 	size_t length = 0;
 	size_t i = 0;
 	while (data[length] != '\0') {
@@ -14,14 +28,100 @@ explicit BasicString::BasicString(const char* const data) {
 	}
 }
 
-BasicString::BasicString(const BasicString& copied) : m_size(copied.m_size) {
-	size_t i = 0;
-	m_data = new char[m_size];
-	for (i = 0; i < m_size; i++) {
-		m_data[i] = copied.m_data[i];
-	}
+BasicString::BasicString(const BasicString& copied) {
+	this->copyBasicString(copied);
 }
 
 BasicString::~BasicString() {
 	delete[] m_data;
+}
+
+BasicString& BasicString::operator=(const BasicString& copied) {
+	this->copyBasicString(copied);
+	return *this;
+}
+
+bool BasicString::operator==(const BasicString& other) const {
+	size_t i = 0;
+	while ((m_data[i] != '\0') && (other.m_data[i] != '\0')) {
+		if (m_data[i] != other.m_data[i]) {
+			return false;
+		}
+		i++;
+	}
+	return true;
+}
+
+BasicString BasicString::operator+(const BasicString& other) const {
+	size_t i = 0;
+	char* new_data = new char [m_size + other.m_size - 1];
+	for (i = 0; i < m_size - 1; i++) {
+		new_data[i] = m_data[i];
+	}
+	for (i = 0; i < other.m_size; i++) {
+		new_data[m_size - 1 + i] = other.m_data[i];
+	}
+	BasicString newBasicString(new_data);
+	delete[] new_data;
+	return newBasicString;
+}
+
+void BasicString::operator+=(const BasicString& other) {
+	size_t i = 0;
+	char* new_data = new char[m_size + other.m_size - 1];
+	for (i = 0; i < m_size - 1; i++) {
+		new_data[i] = m_data[i];
+	}
+	for (i = 0; i < other.m_size; i++) {
+		new_data[m_size - 1 + i] = other.m_data[i];
+	}
+	delete[] m_data;
+	m_data = new_data;
+	m_size += (other.m_size - 1);
+}
+
+BasicString BasicString::operator*(const size_t& multiply) const {
+	size_t i = 0;
+	size_t j = 0;
+	size_t new_size = (m_size - 1) * multiply + 1;
+	char* new_data = new char[new_size];
+	for (j = 0; j < multiply; j++) {
+		for (i = 0; i < m_size - 1; i++) {
+			new_data[j * (m_size - 1) + i] = m_data[i];
+		}
+	}
+	new_data[new_size - 1] = '\0';
+	BasicString newBasicString(new_data);
+	delete[] new_data;
+	return newBasicString;
+}
+
+char& BasicString::operator[](const int& index) const {
+	if ((index >= int(m_size) - 1) || (index <= -1 * int(m_size))) {
+		throw IndexOutOfRangeError();
+	}
+	if (index < 0) {
+		return m_data[m_size - 1 + index];
+	}
+	return m_data[index];
+}
+
+size_t BasicString::getLength() const {
+	return m_size - 1;
+}
+
+void BasicString::printBasicString() const {
+	cout << m_data << endl;
+}
+
+BasicStringError::BasicStringError(const BasicString& errorMessage) : m_errorMessage(errorMessage) {
+	// Left blank intentionally
+}
+
+BasicString BasicStringError::getErrorMessage() const {
+	return m_errorMessage;
+}
+
+IndexOutOfRangeError::IndexOutOfRangeError() : BasicStringError(BasicString ("Index out of range")) {
+	// Left blank intentionally
 }
--- StringBasic/BasicString.h
+++ StringBasic/BasicString.h
@@ -5,11 +5,18 @@ private:
 	char* m_data;
 	size_t m_size;
 
+	/**
+	 * @brief						Create size and data of a Basic String
+	 * 
+	 * @param				[IN]	The Basic String to be copied
+	 */
+	void copyBasicString(const BasicString& copied);
+
 public:
 	/**
 	 * @brief						Basic String constructor
 	 *
-	 * @param data			[IN]	The data of the string	
+	 * @param data			[IN]	The data of the Basic String	
 	 */
 	explicit BasicString(const char* const data);
 
@@ -20,7 +27,71 @@ public:
 	 */
 	BasicString(const BasicString& copied);
 
-	
 	// Basic String destructor
 	~BasicString();
+
+	/**
+	* @brief						Basic String copy assigment operator - performs deep copy
+	*
+	* @param copied		[IN]		The Basic String to be copied
+	*/
+	BasicString& operator=(const BasicString& copied);
+
+	/**
+	 * @brief						'==' operator - Compare Basic Strings
+	 * 
+	 * @param other		[IN]		The Basic String to compare to
+	 * @return						True if all chars up to the first '\0' are identical, false otherwise
+	 */
+	bool operator==(const BasicString& other) const;
+
+	/**
+	 * @brief						'+' operator - Concatenate Basic Strings
+	 *
+	 * @param other		[IN]		The Basic String to add
+	 * @return						New Basic String that is the concatenation of this string and the other string
+	 */
+	BasicString operator+(const BasicString& other) const;
+
+	/**
+	 * @brief						'+=' operator - Append Basic String
+	 * 
+	 * @param other		[IN]		The Basic String to 
+	 */
+	void operator+=(const BasicString& other);
+
+	/**
+	 * @brief						'*' operator - Concatenate Basic String with itself
+	 * 
+	 * @param multiply	[IN]		The number of times to concatenate the Basic String with itself
+	 * @return						The Basic String concatenated with itself mult times
+	 */
+	BasicString operator*(const size_t& multiply) const;
+	
+	/**
+	* @brief						'[]' operator - Access Basic String data by index
+	*
+	* @param index		[IN]		The index
+	* @return						The char at that index
+	*/
+	char& operator[](const int& index) const;
+
+	// Get Basic String Length
+	size_t getLength() const;
+
+	// Print Basic String
+	void printBasicString() const;
+};
+
+class BasicStringError {
+private:
+	BasicString m_errorMessage;
+public:
+	explicit BasicStringError(const BasicString& errorMessage);
+	BasicString getErrorMessage() const;
+};
+
+class IndexOutOfRangeError final : public BasicStringError {
+public:
+	explicit IndexOutOfRangeError();
 };
